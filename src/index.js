31bc1844e5205f28e4afbea031cdc530ac2e334c
Spencer Ng <spencerng@spencers-mbp.lan>
2021-03-10 18:53:02 -0800

    Implemented redux environment setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import store from './redux/store/index.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
 
