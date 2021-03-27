b2a9c7b872b34743c5ac8a5593fbc7fff9b7e181
Spencer Ng <spencerng@spencers-mbp.lan>
2021-03-26 19:27:04 -0700

    Updated axios config

--- src/server/axiosConfig.js
+++ src/server/axiosConfig.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 // Production URL
 const app = axios.create({
-    baseURL: "http://heroku-board-app.com"
+    baseURL: "https://heroku-board-app.com"
 });
 
 // axios consumes rejected API responses by default,
