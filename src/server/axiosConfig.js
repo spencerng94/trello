edc9753044d34112446b21f67b655f7bf47c00ae
Spencer Ng <spencerng@spencers-mbp.lan>
2021-03-26 22:24:33 -0700

    Updated package.json

--- src/server/axiosConfig.js
+++ src/server/axiosConfig.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 // Production URL
 const app = axios.create({
-    baseURL: ""
+    baseURL: "https://trello-board-app.herokuapp.com"
 });
 
 // axios consumes rejected API responses by default,
