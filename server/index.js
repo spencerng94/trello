6092e8c71c1f1ac2c5325ed6b36d13bf5b219785
Spencer Ng <spencerng@spencers-mbp.lan>
2021-03-29 19:08:44 -0700

    Updated build path

--- server/index.js
+++ server/index.js
@@ -74,9 +74,9 @@ app.use("", routes);
 // });
  
 // Use static build files
-app.use(express.static('../../build'))
+app.use(express.static('../../client/build'))
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname,  "../../build", "index.html"));
+  res.sendFile(path.resolve(__dirname,  "../../client/build", "index.html"));
 });
 
 
