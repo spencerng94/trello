3d89dd7f7437da51a76e1cf8b7dbf267b32d7b4a
Spencer Ng <spencerng@spencers-mbp.lan>
2021-03-29 17:42:34 -0700

    Updated CORS

--- src/server/index.js
+++ src/server/index.js
@@ -14,11 +14,24 @@ const PORT = process.env.PORT || 3001;
 
 app.set('port', PORT)
 
-app.use(cors({ origin: true }));
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  }
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('tiny'));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "https://trello-board-app.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+  });
 
 const MONGODB_URI = `mongodb+srv://spencer-2:vcAkCsQyQzH92uM@cluster0.ahwfc.mongodb.net/trello?retryWrites=true&w=majority`
 
